078cd8c8a652250e022a49eaf27d4df275ca1e5f
calebanagnos-sys <caleb.x.anagnos-1@ou.edu>
2025-10-23 10:52:55 -0500

    firedata

---
+++ R/Firedam.R
@@ -0,0 +1,12 @@
+#' Fire Damage Data
+#'
+#' A dataset containing information about fire damage (from FIREDAM.csv).
+#'
+#' @format ## `fire`
+#' A data frame with 2 rows and 15 columns:
+#' \describe{
+#'   \item{Distance}{The distance the fire spread}
+#'   \item{Damage}{The damage of the fire}
+#' }
+#' @source data from Lab 7.
+"fire"
